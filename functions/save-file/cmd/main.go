2a046317abd19aef01fa4d7b774335a298724abf
Caio Aureliano <caio.aureliano1@gmail.com>
2023-01-17 22:45:31 -0300

    chore(*): init go module with test handler to object created s3 bucket event

---
+++ functions/save-file/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, event events.S3Event) {
+	println("event")
+	fmt.Printf("%v", event)
+}
---
+++ functions/save-file/go.mod
@@ -0,0 +1,5 @@
+module github.com/CaioAureliano/image-linker/save-file
+
+go 1.18
+
+require github.com/aws/aws-lambda-go v1.37.0
